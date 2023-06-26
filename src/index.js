403f374fcc239c92146d46f1391c0b997e886fbf
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-26 14:37:53 -0700

    Add code

--- src/index.js
+++ src/index.js
@@ -5,40 +5,97 @@ import './css/styles.css';
 // Business Logic
 
 function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}
+    `;
 
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city);
-    }
-  });
+    request.addEventListener("loadend", function() {
+        try {
+            const response = JSON.parse(this.responseText);
+            const lon = response.coord.lon;
+            const lat = response.coord.lat;
+            printElements(response, city);
+            getAirPollution(lon, lat, city); // Pass lon and lat to getAirPollution()
+        } catch (error) {
+            printError(this, error, city);
+        }
+    });
 
-  request.open("GET", url, true);
-  request.send();
+    request.open("GET", url, true);
+    request.send();
 }
+
+function getAirPollution (lon, lat, city) {
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`;
+    request.addEventListener("loadend", function() {
+        try {
+            const response = JSON.parse(this.responseText);
+            printPollutionElements(response, city);
+        } catch (error) {
+            printError(this, error, city);
+        }
+    });
+
+    request.open("GET", url, true);
+    request.send();
+}
+
 
 // UI Logic
 
 function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%. 
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+    document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
+    The temperature in Kelvins is ${apiResponse.main.temp} degrees.
+    The temperature in Fahrenheit is ${((apiResponse.main.temp - 273.15) * 9/5 + 32).toFixed(2)} degrees.
+    Feels like ${((apiResponse.main.feels_like - 273.15) * 9/5 + 32).toFixed(0)} degrees.
+    With ${apiResponse.weather[0].description}
+    And a high of ${((apiResponse.main.temp_max - 273.15) * 9/5 + 32).toFixed(0)} degrees.
+    
+    Sunrise: ${new Date(apiResponse.sys.sunrise * 1000).toLocaleTimeString("en-US")}
+    Sunset: ${new Date(apiResponse.sys.sunset * 1000).toLocaleTimeString("en-US")}`;
+}
+
+function getAqiDescription(aqi) {
+    switch (aqi) {
+    case 1:
+        return "Good";
+    case 2:
+        return "Fair";
+    case 3:
+        return "Moderate";
+    case 4:
+        return "Poor";
+    case 5:
+        return "Very Poor";
+    default:
+        return "Unknown";
+    }
+}
+
+function printPollutionElements(apiResponse, city) {
+    let aqi = apiResponse.list[0].main.aqi;
+    let aqiQualitiveName = getAqiDescription(aqi);
+    // document.querySelector('#showairResponse').innerText = `Air Quality in ${city} is ${apiResponse.list[0].main.aqi}.
+    // `;
+    document.querySelector('#showairResponse').innerText = `Air Quality in ${city} is ${aqiQualitiveName}.
+    `;
 }
 
 function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+    document.querySelector('#showResponse').innerHTML = `There was an error accessing the weather data for "${city}"
+    <br>
+    <strong>${request.status}</strong> | <strong>${request.statusText}:</strong> <em>${apiResponse.message}</em>`;
 }
 
-function handleFormSubmission(event) {
-  event.preventDefault();
-  const city = document.querySelector('#location').value;
-  document.querySelector('#location').value = null;
-  getWeather(city);
+function handleWeatherFormSubmission(event) {
+    event.preventDefault();
+    const city = document.querySelector('#location').value;
+    document.querySelector('#location').value = null;
+    getWeather(city);
+    document.getElementById("air-pol-stats").removeAttribute("class", "hidden");
 }
 
 window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+    document.querySelector('#weather-form').addEventListener("submit", handleWeatherFormSubmission);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -5,34 +5,34 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devtool: 'eval-source-map',
-  devServer: {               
-    contentBase: './dist'    
-  },
-  plugins: [
-    new ESLintPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
-      template: './src/index.html',
-      inject: 'body'
-    }),
-    new Dotenv()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'eval-source-map',
+    devServer: {               
+        contentBase: './dist'    
+    },
+    plugins: [
+        new ESLintPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Shape Tracker',
+            template: './src/index.html',
+            inject: 'body'
+        }),
+        new Dotenv()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
         ]
-      }
-    ]
-  }
+    }
 };
